9607a07e0016195751ea954350b9270707512a3b
zaid-sinpie <zaidmasuldar@gmail.com>
2024-07-07 22:09:58 +0530

    colors added in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary:"131842",
+      secondary:"#45474B",
+      actionColor: "#E9FF97",
+    },
   },
   plugins: [],
-}
+};
