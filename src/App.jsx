15b700366e1d22bd04646082ccbeffad5b8dbf0f
Dilip Dandi <dilipdandi861@gmail.com>
2024-07-09 21:23:43 +0530

    contact page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
+import Contact from "./components/Contact";
 function App() {
   return (
     <div className="flex items-center justify-center h-[100vh] w-full bg-primary">
       
-     
+     <Contact/>
      
     </div>
   );
