ec1bd2da16252a56f0b55cf62566611f6a689729
zaid-sinpie <zaidmasuldar@gmail.com>
2024-07-09 19:57:16 +0530

    app jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import Buttons from "./components/ui/Buttons.jsx"
-import Certification from "./components/Certification";
 function App() {
   return (
-    <div className="flex items-center justify-center h-[100vh] w-full bg-primary">
-      <Certification/>
-      <Buttons>Login</Buttons>
-   
-    </div>
+    <div className="flex items-center justify-center h-[100vh] w-full bg-primary"></div>
   );
 }
 
