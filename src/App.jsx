d18748c8c2836fa5d81ce2822ca80d9a493ae750
Abhi-sinpie <abhishekmudata@gmail.com>
2024-07-09 20:09:35 +0530

    Changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import Buttons from "./components/ui/Buttons.jsx"
-import Certification from "./components/Certification.jsx";
-import Projects from "./components/Projects.jsx"
+
 
 function App() {
   return (
     <div className="flex items-center justify-center h-[100vh] w-full bg-primary">
-      {/* <Certification/> */}
-      <Projects/>
-      {/* <Buttons>Login</Buttons> */}
-      
+     
    
     </div>
   );
