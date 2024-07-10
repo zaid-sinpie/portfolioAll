5ce5d8d3bc9ea21746a8b95e728ee26755795478
zaid-sinpie <zaidmasuldar@gmail.com>
2024-07-10 20:42:20 +0530

    pulling all pushes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="flex flex-col items-center justify-start h-full w-full bg-primary"></div>
+    <div className="flex flex-col items-center justify-start h-full w-full bg-primary">
+      
+    </div>
   );
 }
 
-export default App;
+export default App; 
